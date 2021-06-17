6b17316e73170af9bc63057399659d158733ae63
thoriqnoer <thoriq.noer@gmail.com>
2021-06-17 15:34:57 +0700

    First commit

---
+++ models/campground.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const Review = require('./review')
+const Schema = mongoose.Schema
+
+// https://res.cloudinary.com/dppjuascu/image/upload/w_300/v1620014297/YelpCamp/ybvksyexayyxyyaijl8n.jpg
+
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+})
+
+ImageSchema.virtual('thumbnail').get(function () {
+  return this.url.replace('/upload', '/upload/w_150')
+})
+
+const CampgroundSchema = new Schema({
+  title: String,
+  images: [ImageSchema],
+  price: Number,
+  description: String,
+  location: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Review',
+    },
+  ],
+})
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    })
+  }
+})
+
+module.exports = mongoose.model('Campground', CampgroundSchema)
---
+++ seeds/index.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+const cities = require('./cities')
+const { places, descriptors } = require('./seedHelper')
+const Campground = require('../models/campground')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'Connection error:'))
+db.once('open', () => {
+  console.log('Database connected')
+})
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)]
+
+const seedDb = async () => {
+  await Campground.deleteMany({})
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000)
+    const price = Math.floor(Math.random() * 20) + 10
+    const camp = new Campground({
+      author: '6087876c7710281af4b83d69',
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid id et debitis in, quae provident facilis nobis minus numquam quasi ea expedita, molestias aperiam cum odio, quaerat architecto rerum veritatis!',
+      price,
+      images: [
+        {
+          url:
+            'https://res.cloudinary.com/dppjuascu/image/upload/v1619934096/YelpCamp/oktgwfyuw6idheolrdvq.jpg',
+          filename: 'YelpCamp/oktgwfyuw6idheolrdvq',
+        },
+        {
+          url:
+            'https://res.cloudinary.com/dppjuascu/image/upload/v1619934096/YelpCamp/da1kwmqdaoldr2ktn3qr.jpg',
+          filename: 'YelpCamp/da1kwmqdaoldr2ktn3qr',
+        },
+      ],
+    })
+    await camp.save()
+  }
+}
+
+seedDb().then(() => {
+  mongoose.connection.close()
+})
