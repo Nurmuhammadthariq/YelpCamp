72eed6649524de07243407b5a97cde5ddcf988ed
thoriqnoer <thoriq.noer@gmail.com>
2021-06-19 11:36:27 +0700

    Fixing limit for forwardGeocode

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -17,11 +17,11 @@ module.exports.createCampground = async (req, res, next) => {
   const geoData = await geocoder
     .forwardGeocode({
       query: req.body.campground.location,
-      limit: 2,
+      limit: 1,
     })
     .send()
   const campground = new Campground(req.body.campground)
-  campground.geometry = geoData.body.features[1].geometry
+  campground.geometry = geoData.body.features[0].geometry
   campground.images = req.files.map((f) => ({
     url: f.path,
     filename: f.filename,
